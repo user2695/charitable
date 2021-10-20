1cf08935e17dc1fd00d207242c4f05291c7363f1
shakesbeer2049 <max.zubi@gmail.com>
2021-10-20 11:32:16 +0530

    admin-login

---
+++ models/Admin.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const AdminSchema = new mongoose.Schema(
+    {
+        email: {
+          type : String,
+          unique: true,
+        },
+        password: {
+            type: String,
+        }
+    }
+)
+module.exports = mongoose.model("Admin", AdminSchema)
